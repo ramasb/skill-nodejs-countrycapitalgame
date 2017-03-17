4667c3e80d865672453e2c1205e095da089cec1e
balaji <balaji_ramasamy@yahoo.com>
2017-03-17 11:16:49 -0400

    Initial

--- skill-sample-nodejs-highlowgame/src/index.js
+++ skill-sample-nodejs-highlowgame/src/index.js
@@ -5,7 +5,7 @@ var appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = appId;
-    alexa.dynamoDBTableName = 'highLowGuessUsers';
+    alexa.dynamoDBTableName = 'countryCapitals';
     alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers, guessAttemptHandlers);
     alexa.execute();
 };
@@ -19,11 +19,12 @@ var newSessionHandlers = {
     'NewSession': function() {
         if(Object.keys(this.attributes).length === 0) {
             this.attributes['endedSessionCount'] = 0;
-            this.attributes['gamesPlayed'] = 0;
+            this.attributes['guessPlayed'] = 0;
+            this.attributes['guessWon'] = 0;
         }
         this.handler.state = states.STARTMODE;
-        this.emit(':ask', 'Welcome to High Low guessing game. You have played '
-            + this.attributes['gamesPlayed'].toString() + ' times. would you like to play?',
+        this.emit(':ask', 'Welcome to Country-Capital guessing game. You have played '
+            + this.attributes['guessPlayed'].toString() + ' times. would you like to play?',
             'Say yes to start the game or no to quit.');
     },
     "AMAZON.StopIntent": function() {
@@ -44,14 +45,18 @@ var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
         this.emit('NewSession'); // Uses the handler in newSessionHandlers
     },
     'AMAZON.HelpIntent': function() {
-        var message = 'I will think of a number between zero and one hundred, try to guess and I will tell you if it' +
-            ' is higher or lower. Do you want to start the game?';
+        var message = 'I will say a country, please find the capital for that ' +
+            ' country. Do you want to start the game?';
         this.emit(':ask', message, message);
     },
     'AMAZON.YesIntent': function() {
-        this.attributes["guessNumber"] = Math.floor(Math.random() * 100);
+    	this.attributes['guessPlayed']++;
+    	var country = "India";
+    	var capital = "New Delhi";
+        this.attributes["country"] = country;
+        this.attributes["capital"] = capital;
         this.handler.state = states.GUESSMODE;
-        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
+        this.emit(':ask', 'Great! ' + 'What is the capital for ' + this.attributes["country"] + '?', 'Capital.');
     },
     'AMAZON.NoIntent': function() {
         console.log("NOINTENT");
@@ -82,31 +87,32 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
         this.handler.state = '';
         this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
     },
-    'NumberGuessIntent': function() {
-        var guessNum = parseInt(this.event.request.intent.slots.number.value);
-        var targetNum = this.attributes["guessNumber"];
-        console.log('user guessed: ' + guessNum);
-
-        if(guessNum > targetNum){
-            this.emit('TooHigh', guessNum);
-        } else if( guessNum < targetNum){
-            this.emit('TooLow', guessNum);
-        } else if (guessNum === targetNum){
+    'CapitalGuessIntent': function() {
+        var capital = parseInt(this.event.request.intent.slots.capital);
+        var country = this.attributes["country"];
+        var oCapital = this.attributes["capital"];
+        console.log('user guessed: ' + capital);
+        if(capital == oCapital){
             // With a callback, use the arrow function to preserve the correct 'this' context
-            this.emit('JustRight', () => {
-                this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
+            this.emit('CapitalRight', () => {
+                this.emit(':ask', capital + 'is correct! Would you like to play a new game?',
                 'Say yes to start a new game, or no to end the game.');
-        })
+            });
         } else {
-            this.emit('NotANum');
+            this.emit('CapitalWrong');
         }
     },
-    'AMAZON.HelpIntent': function() {
-        this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
-            ' if it is higher or lower.', 'Try saying a number.');
+    'CapitalClueIntent': function() {
+    	var startLetter = this.attributes["capital"].substring(0,1);
+        this.emit(':ask', 'Clue is the capital for ' + this.attributes["country"] + '. Capital starts with letter ' + startLetter, 'Capital.');
     },
+    'AMAZON.HelpIntent': function() {
+        this.emit(':ask', 'What is the capital for ' + this.attributes["country"] + '?', 'Capital.');
+    },
+    
     "AMAZON.StopIntent": function() {
         console.log("STOPINTENT");
+        this.emit(':tell', "You have won" + this.attributes['guessWon'] + " out of " + this.attributes['guessPlayed'] + " games played!");  
       this.emit(':tell', "Goodbye!");  
     },
     "AMAZON.CancelIntent": function() {
@@ -119,24 +125,21 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
     },
     'Unhandled': function() {
         console.log("UNHANDLED");
-        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying the capital again.', 'Try saying a capital.');
     }
 });
 
 // These handlers are not bound to a state
 var guessAttemptHandlers = {
-    'TooHigh': function(val) {
-        this.emit(':ask', val.toString() + ' is too high.', 'Try saying a smaller number.');
-    },
-    'TooLow': function(val) {
-        this.emit(':ask', val.toString() + ' is too low.', 'Try saying a larger number.');
-    },
-    'JustRight': function(callback) {
+    'CapitalRight': function(callback) {
         this.handler.state = states.STARTMODE;
-        this.attributes['gamesPlayed']++;
+        this.attributes['guessWon']++;
         callback();
     },
-    'NotANum': function() {
-        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+    'CapitalWrong': function() {
+        this.emit(':ask', 'Sorry, wrong answer.', 'Try saying again.');
+    },
+    'Wrong': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying again.', 'Try saying again.');
     }
 };
